d9d5a088dd33139ea2d58135ce75b5c55b819051
Lucas Lodi Kreusberg <gitkreusberg@gmail.com>
2024-10-22 23:23:30 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+
+    int qtdTurmas;
+    int qtdAlunos;
+    float mediaAtual;
+    float maiorMedia;
+    int turmaComMaiorMedia;
+    int somaNotasAtuais;
+    int notasTurma[100];
+
+    // solicita a quantidade de turmas
+    printf("Informe a quantidade de turmas: ");
+    scanf("%d", &qtdTurmas);
+
+    // solicita a quantidade de alunos
+    for (int i = 1; i <= qtdTurmas; i++) {
+        printf("Digite a quantidade de alunos da turma %d: ", i);
+        scanf("%d", &qtdAlunos);
+
+        // solicita a nota individual de cada aluno
+        for (int j = 1; j <= qtdAlunos; j++) {
+            printf("Digite a nota do aluno %d: ", j);
+            scanf("%d", &notasTurma[j]);
+            somaNotasAtuais += notasTurma[j];
+            printf("== somaNotasAtuais == %d \n", somaNotasAtuais);
+        }
+
+        printf("== sizeof(qtdAlunos) == %i \n", qtdAlunos);
+        mediaAtual = somaNotasAtuais / qtdAlunos;
+        printf("== mediaAtual == %3.2f \n", mediaAtual);
+
+        somaNotasAtuais = 0;
+
+        if (mediaAtual > maiorMedia) {
+            maiorMedia = mediaAtual;
+            turmaComMaiorMedia = i;
+        }
+        printf("== somaNotasAtuais == %d \n", somaNotasAtuais);
+    }
+
+    printf("Ao fim do programa, a turma com maior media eh a turma numero %d", turmaComMaiorMedia);
+
+    return 0;
+}
---
+++ todo.md
@@ -0,0 +1,11 @@
+TODO:
+
+* aceitar um inteiro sendo a quantidade de turmas
+* aceitar um inteiro sendo a quantidade de alunos de cada turma
+* salvar as notas de cada aluno e tirar a média deles
+* imprimir na tela a melhor média
+* traduzir tudo para Portugol
+
+Anotações:
+
+Como não sabemos quantos alunos serão, podemos solicitar de antemão, e com o resultado, por dentro do array. Desta forma não gastaremos espaço adicional desnecessáriamente na memória
